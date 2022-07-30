5fa3b50488dc2f15313621f237a4993e3c5967e5
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-30 23:29:37 +0530

    second smallest element

---
+++ prog_8.c
@@ -0,0 +1,28 @@
+/**Write a program to find the second smallest number in an array.Take array values from the user*/
+
+#include <stdio.h>
+
+int main() {
+    int a[10], i, j, temp;
+    printf("Enter all 10 elements: ");
+    for(i = 0; i<10; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    for(i = 0; i<9; i++)
+    {
+        for(j = i+1; j<10; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("second smallest element in array is: %d\n", a[1]);
+    
+    return 0;
+}
