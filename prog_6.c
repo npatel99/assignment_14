653b293454d5abb75aa87d8faf2ce3adac773c3a
npatel99 <50912904+npatel99@users.noreply.github.com>
2022-07-30 23:07:12 +0530

    sorting of array

---
+++ prog_6.c
@@ -0,0 +1,33 @@
+/**Write a program to sort elements of an array of size 10. Take array values from the user.*/
+
+#include <stdio.h>
+
+int main() {
+    int a[10], i, j, temp;
+    printf("Enter all 10 elements: ");
+    for(i = 0; i<10; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    for(i = 0; i<9; i++)
+    {
+        for(j = i+1; j<10; j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("sorted array is: ");
+    for(i = 0; i<10; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
